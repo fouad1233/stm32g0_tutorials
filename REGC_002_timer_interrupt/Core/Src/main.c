60b5f5097d0a994f590e4a74f8fc03e9b5cf339b
Fouad Aladhami <fouadiadhami@gmail.com>
2023-12-02 16:27:10 +0300

    REGC_002_timer_interrupt interrupt worked but without EXTI

--- REGC_002_timer_interrupt/Core/Src/main.c
+++ REGC_002_timer_interrupt/Core/Src/main.c
@@ -2,17 +2,23 @@
 #include "stm32g031xx.h"
 
 
-
+//defines
+#define TIMERPSC 16000
+#define TIMERPERIYOD 1000
 //variables
 uint64_t tick;
-uint8_t seconds;
+uint64_t seconds;
+uint8_t buttonCounter = 1;
+uint8_t buttonPreviousState;
+uint8_t buttonNewState;
 
 
 
 //functions prototypes
-void GPIOC_RCC_Init(void);
+void GPIO_RCC_Init(void);
 void GPIOC_Init(void);
-void toggle_led(void);
+void GPIOA_Init(void);
+
 
 void TIM2_Clock_Init(void);
 void TIM2_Interrupt_Config(void);
@@ -20,23 +26,41 @@ void TIM2_IRQHandler(void);
 void Start_TIM2(void);
 void Stop_TIM2(void);
 
+void toggle_led(void);
+uint8_t readButton(void);
 
 int main(void) {
 
 	//init
-	GPIOC_RCC_Init();
+	GPIO_RCC_Init();
+	GPIOA_Init();
 	GPIOC_Init();
 	TIM2_Clock_Init();
 	TIM2_Interrupt_Config();
+
+
 	Start_TIM2();
+
 
 	while(1) {
 
+		buttonNewState = readButton();
+		if (buttonPreviousState == 0 && buttonNewState == 1){
+			buttonCounter++;
+			if (buttonCounter > 10) {
+				buttonCounter = 1;
+			}
+			TIM2_Clock_Init();
+		}
+		buttonPreviousState = buttonNewState;
     }
 
     return 0;
 }
+uint8_t readButton(void){
 
+	return GPIOA->IDR & 1;
+}
 void TIM2_IRQHandler(void) {
     if (TIM2->SR & TIM_SR_UIF) {
         //Code here
@@ -47,14 +71,34 @@ void TIM2_IRQHandler(void) {
         TIM2->SR &= ~TIM_SR_UIF;
     }
 }
-void GPIOC_RCC_Init(void){
+
+
+void GPIO_RCC_Init(void){
 	 /* Enable GPIOC clock */
 	RCC->IOPENR |= (1U << 2);
+
+	// Enable clock for GPIOA
+	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+
+	// Enable clock for SYSCFG
+	RCC->APBENR2 |= RCC_APBENR2_SYSCFGEN;
 }
 void GPIOC_Init(void){
+	//Configure GPIO for board on led
 	GPIOC->MODER &= ~(3U << 2*6);
 	GPIOC->MODER |= (1U << 2*6);
 }
+void GPIOA_Init(void){
+	// Set GPIOA Pin 0 as input
+	GPIOA->MODER &= ~GPIO_MODER_MODE0; // Clear mode bits for pin 0
+
+	// Enable pull-down resistor for GPIOA Pin 0
+	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD0; // Clear pull-up/pull-down bits for pin 0
+	GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_1; // Set bit for pull-down
+
+}
+
+
 void toggle_led(void){
 	GPIOC->ODR ^= (1U << 6);
 }
@@ -63,8 +107,8 @@ void TIM2_Clock_Init(void){
 	RCC->APBENR1 |= RCC_APBENR1_TIM2EN;
 
 	// Set TIM2 prescaler and period
-	TIM2->PSC = 15999;
-	TIM2->ARR = 999;
+	TIM2->PSC = TIMERPSC - 1;
+	TIM2->ARR = (TIMERPERIYOD *buttonCounter) - 1;
 }
 void TIM2_Interrupt_Config(void){
 	NVIC_SetPriority(TIM2_IRQn, 15);
