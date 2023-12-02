46fe93136bd7c50bab9a3f6957632ee74572c421
Fouad Aladhami <fouadiadhami@gmail.com>
2023-12-02 12:41:51 +0300

    delay function using systick implemented

--- REGC_001_ledblink_systick/Core/Src/main.c
+++ REGC_001_ledblink_systick/Core/Src/main.c
@@ -1,30 +1,51 @@
 #include "stm32g0xx.h"
+#include "stm32g031xx.h"
 
 #define LEDDELAY    1600000
 
+//variables
+uint64_t tick;
+uint8_t seconds;
+
+
+
+//functions prototypes
 void delay(volatile uint32_t);
+void Init_Systick(uint32_t tick);
+void Systick_Handler(void);
+void increase_tick(void);
+uint64_t getTick(void);
+void delay_ms(uint64_t msvalue);
 
 int main(void) {
 
-    /* Enable GPIOC clock */
-    RCC->IOPENR |= (1U << 2);
+	//init
+	Init_Systick(16000);
 
-    /* Setup PC6 as output */
-    GPIOC->MODER &= ~(3U << 2*6);
-    GPIOC->MODER |= (1U << 2*6);
-
-    /* Turn on LED */
-    GPIOC->ODR |= (1U << 6);
-
-    while(1) {
-        delay(LEDDELAY);
-        /* Toggle LED */
-        GPIOC->ODR ^= (1U << 6);
+	while(1) {
+        delay_ms(1000);
+        seconds ++;
     }
 
     return 0;
 }
+void increase_tick(void){
+	tick++;
 
-void delay(volatile uint32_t s) {
-    for(; s>0; s--);
 }
+void Init_Systick(uint32_t tick){
+	SysTick->LOAD = tick; // Count down from 999 to 0
+	SysTick->VAL  = 0;   // Clear current value
+	SysTick->CTRL = 0x7; // Enable Systick, exception,and use processor clock
+}
+uint64_t getTick(void){
+	return tick;
+}
+void delay_ms(uint64_t msvalue){
+	uint64_t startTick =getTick() ;
+	while ((getTick() - startTick) < msvalue)
+	  {
+	  }
+
+}
+
