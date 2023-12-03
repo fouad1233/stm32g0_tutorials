3e19a8d9c69218da6ab75aafeed9b55a6b040006
Fouad Aladhami <fouadiadhami@gmail.com>
2023-12-03 08:44:32 +0300

    EXTI code added but not work success

--- REGC_002_timer_interrupt/Core/Src/main.c
+++ REGC_002_timer_interrupt/Core/Src/main.c
@@ -29,6 +29,9 @@ void Stop_TIM2(void);
 void toggle_led(void);
 uint8_t readButton(void);
 
+void EXTI_Init(void);
+void EXTI0_1_IRQHandler(void);
+
 int main(void) {
 
 	//init
@@ -37,6 +40,7 @@ int main(void) {
 	GPIOC_Init();
 	TIM2_Clock_Init();
 	TIM2_Interrupt_Config();
+	EXTI_Init();
 
 
 	Start_TIM2();
@@ -58,21 +62,48 @@ int main(void) {
 
     return 0;
 }
-uint8_t readButton(void){
 
-	return GPIOA->IDR & 1;
-}
 void TIM2_IRQHandler(void) {
     if (TIM2->SR & TIM_SR_UIF) {
         //Code here
-    	seconds++;
+    	seconds +=  buttonCounter;
     	toggle_led();
 
         // Clear the interrupt flag
         TIM2->SR &= ~TIM_SR_UIF;
     }
 }
+void EXTI0_1_IRQHandler(void) {
+    // Your code to handle the EXTI interrupt for PA0 goes here
+    // This is where you can perform specific actions when the rising edge occurs
+	if (EXTI->RPR1 & EXTI_FPR1_FPIF0) // Check if the interrupt is from line 0
+		{
+		buttonCounter = 100;
+		// Clear EXTI0 pending flag
+		EXTI->RPR1 |= EXTI_FPR1_FPIF0;
+		}
+}
+uint8_t readButton(void){
 
+	return GPIOA->IDR & 1;
+}
+void EXTI_Init(void) {
+
+
+    // Connect EXTI0 to GPIOA Pin 0
+    EXTI->EXTICR[0] |= EXTI_EXTICR1_EXTI0_0;
+
+    // Configure EXTI0 to rising edge trigger
+    EXTI->RTSR1 |= EXTI_RTSR1_RT0;
+    EXTI->FTSR1 &= ~EXTI_FTSR1_FT0;
+
+    // Enable EXTI0 interrupt
+    EXTI->IMR1 |= EXTI_IMR1_IM0;
+
+    // Set the EXTI0 interrupt priority (adjust as needed)
+    NVIC_SetPriority(EXTI0_1_IRQn, 1);
+    NVIC_EnableIRQ(EXTI0_1_IRQn);
+}
 
 void GPIO_RCC_Init(void){
 	 /* Enable GPIOC clock */
@@ -83,6 +114,9 @@ void GPIO_RCC_Init(void){
 
 	// Enable clock for SYSCFG
 	RCC->APBENR2 |= RCC_APBENR2_SYSCFGEN;
+
+	// Enables GPIOA peripheral
+	RCC->AHBENR |= 1;
 }
 void GPIOC_Init(void){
 	//Configure GPIO for board on led
@@ -94,8 +128,8 @@ void GPIOA_Init(void){
 	GPIOA->MODER &= ~GPIO_MODER_MODE0; // Clear mode bits for pin 0
 
 	// Enable pull-down resistor for GPIOA Pin 0
-	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD0; // Clear pull-up/pull-down bits for pin 0
-	GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_1; // Set bit for pull-down
+	//GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD0; // Clear pull-up/pull-down bits for pin 0
+	//GPIOA->PUPDR |= GPIO_PUPDR_PUPD0_1; // Set bit for pull-down
 
 }
 
@@ -109,7 +143,7 @@ void TIM2_Clock_Init(void){
 
 	// Set TIM2 prescaler and period
 	TIM2->PSC = TIMERPSC - 1;
-	TIM2->CNT = (TIMERPERIYOD *buttonCounter) - 1;
+	TIM2->CNT = 0;
 	TIM2->ARR = (TIMERPERIYOD *buttonCounter) - 1;
 }
 void TIM2_Interrupt_Config(void){
