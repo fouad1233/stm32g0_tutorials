16b87da11cadba9570ec8fe02d2d303fd34d6cb3
busraulker <busraulker2929@gmail.com>
2023-12-12 11:19:26 +0300

    KEYPAD

--- REGC_008_withkeypad/Core/Src/main.c
+++ REGC_008_withkeypad/Core/Src/main.c
@@ -4,11 +4,11 @@
 void clearRowsKeypad(void);
 void setRowsKeypad (void);
 void storenumber(int sayı);
-uint32_t number;
-uint32_t count;
+uint32_t number;//number of stored digits
+uint32_t count;//duty cycle rate
 uint32_t n;
 uint32_t ust;
-int numbers[]={};
+int numbers[]={0,0,0,0,0};//store digit
 
 void EXTI4_15IRQHandler(void){
 
@@ -16,33 +16,33 @@ void EXTI4_15IRQHandler(void){
    if((EXTI->RPR1 >> 4) & 1){//Interrupt from PA4
 	   clearRowsKeypad();
 
-	   GPIOA->ODR^=(1U<<0);//PA0
+	   GPIOB->ODR^=(1U<<4);//PB4
 	   if ((GPIOA->IDR>>4)&1){
 		   storenumber(1);
 
 	   }
-	   GPIOA->ODR^=(1U<<0);//PA0
+	   GPIOB->ODR^=(1U<<4);//PB4
 
-	   GPIOA->ODR^=(1U<<1);//PA1
+	   GPIOB->ODR^=(1U<<5);//PB5
 	   if ((GPIOA->IDR>>4)&1){
 		   storenumber(4);
 
 	   }
-	   GPIOA->ODR^=(1U<<1);//PA1
+	   GPIOB->ODR^=(1U<<5);//PB5
 
-	   GPIOA->ODR^=(1U<<2);//PA2
+	   GPIOB->ODR^=(1U<<6);//PB6
 	   if ((GPIOA->IDR>>4)&1){
 		   storenumber(7);
 
 	   }
-	   GPIOA->ODR^=(1U<<2);//PA2
+	   GPIOB->ODR^=(1U<<6);//PB6
 
-	   GPIOA->ODR^=(1U<<3);//PA3
+	   GPIOB->ODR^=(1U<<7);//PB7
 	   if ((GPIOA->IDR>>4)&1){
-		   storenumber(12);
+		   storenumber(11);
 
 	   }
-	   GPIOA->ODR^=(1U<<2);//PA2
+	   GPIOB->ODR^=(1U<<7);//PB7
 
 
        EXTI->RPR1|=(1U<<4);//clear interrupt flag
@@ -52,107 +52,109 @@ void EXTI4_15IRQHandler(void){
    if ((EXTI->RPR1 >> 5) & 1){//Interrupt from PA5
 
 	   clearRowsKeypad();
+	   GPIOB->ODR^=(1U<<4);//PB4
+	   if ((GPIOA->IDR>>5)&1){
+	   	  storenumber(2);
+       }
+	   GPIOB->ODR^=(1U<<4);//PB4
 
-	   	   GPIOA->ODR^=(1U<<0);//PA0
-	   	   if ((GPIOA->IDR>>5)&1){
-	   		   storenumber(2);
+	   GPIOB->ODR^=(1U<<5);//PB5
+	   if ((GPIOA->IDR>>5)&1){
+	      storenumber(5);
+       }
+	   GPIOB->ODR^=(1U<<5);//PB5
 
-	   	   }
-	   	   GPIOA->ODR^=(1U<<0);//PA0
+	   GPIOB->ODR^=(1U<<6);//PB6
+	   if ((GPIOA->IDR>>5)&1){
+	   	  storenumber(8);
 
-	   	   GPIOA->ODR^=(1U<<1);//PA1
-	   	   if ((GPIOA->IDR>>5)&1){
-	   		   storenumber(5);
+	   }
+	   GPIOB->ODR^=(1U<<6);//PB6
 
-	   	   }
-	   	   GPIOA->ODR^=(1U<<1);//PA1
+	   GPIOB->ODR^=(1U<<7);//PB7
+	   if ((GPIOA->IDR>>5)&1){
+	   	  storenumber(0);
 
-	   	   GPIOA->ODR^=(1U<<2);//PA2
-	   	   if ((GPIOA->IDR>>5)&1){
-	   		   storenumber(8);
-
-	   	   }
-	   	   GPIOA->ODR^=(1U<<2);//PA2
-
-	   	   GPIOA->ODR^=(1U<<3);//PA3
-	   	   if ((GPIOA->IDR>>5)&1){
-	   		   storenumber(0);
-
-	   	   }
-	   	   GPIOA->ODR^=(1U<<2);//PA2
+	    }
+	   	GPIOB->ODR^=(1U<<7);//PB7
+	    EXTI->RPR1|=(1U<<5);//clear interrupt flag
+	    setRowsKeypad();
+}
 
 
-       EXTI->RPR1|=(1U<<5);//clear interrupt flag
-       setRowsKeypad();
 
-      }
+
    if ((EXTI->RPR1 >> 6) & 1){//Interrupt from PA6
-
-	   clearRowsKeypad();
-       GPIOA->ODR^=(1U<<0);//PA0
+       clearRowsKeypad();
+       GPIOB->ODR^=(1U<<4);//PB4
 	   if ((GPIOA->IDR>>6)&1){
 		   storenumber(3);
-         }
-	   GPIOA->ODR^=(1U<<0);//PA0
 
-	   GPIOA->ODR^=(1U<<1);//PA1
+	   }
+	   GPIOB->ODR^=(1U<<4);//PB4
+
+	   GPIOB->ODR^=(1U<<5);//PB5
 	   if ((GPIOA->IDR>>6)&1){
 		   storenumber(6);
 
 	   }
-	   GPIOA->ODR^=(1U<<1);//PA1
+	   GPIOB->ODR^=(1U<<5);//PB5
 
-	   GPIOA->ODR^=(1U<<2);//PA2
+	   GPIOB->ODR^=(1U<<6);//PB6
 	   if ((GPIOA->IDR>>6)&1){
 		   storenumber(9);
 
-	   	}
-	    GPIOA->ODR^=(1U<<2);//PA2
+	   }
+	   GPIOB->ODR^=(1U<<6);//PB6
 
-	   	GPIOA->ODR^=(1U<<3);//PA3
-	   	if ((GPIOA->IDR>>6)&1){
-	   		storenumber(10);//like #
+	   GPIOB->ODR^=(1U<<7);//PB7
+	   if ((GPIOA->IDR>>6)&1){
+		   storenumber(11);
 
-	   	}
-	   	GPIOA->ODR^=(1U<<2);//PA2
+	   }
+	   GPIOB->ODR^=(1U<<7);//PB7
 
-        EXTI->RPR1|=(1U<<6);//clear interrupt flag
-        setRowsKeypad();
 
-      }
+	   EXTI->RPR1|=(1U<<6);//clear interrupt flag
+	   setRowsKeypad();
+
+
+
+}
    if ((EXTI->RPR1 >> 7) & 1){//Interrupt from PA7
         clearRowsKeypad();
+        GPIOB->ODR^=(1U<<4);//PB4
+        	   if ((GPIOA->IDR>>7)&1){
+        		   storenumber(11);
 
-	    GPIOA->ODR^=(1U<<0);//PA0
-	   	if ((GPIOA->IDR>>7)&1){
-	   		storenumber(11);
+        	   }
+        	   GPIOB->ODR^=(1U<<4);//PB4
 
-	    }
-	   	GPIOA->ODR^=(1U<<0);//PA0
+        	   GPIOB->ODR^=(1U<<5);//PB5
+        	   if ((GPIOA->IDR>>7)&1){
+        		   storenumber(11);
 
-	   	GPIOA->ODR^=(1U<<1);//PA1
-	    if ((GPIOA->IDR>>7)&1){
-	    	storenumber(11);
+        	   }
+        	   GPIOB->ODR^=(1U<<5);//PB5
 
-	   	}
-	   	GPIOA->ODR^=(1U<<1);//PA1
+        	   GPIOB->ODR^=(1U<<6);//PB6
+        	   if ((GPIOA->IDR>>7)&1){
+        		   storenumber(11);
 
-	   	GPIOA->ODR^=(1U<<2);//PA2
-	   	if ((GPIOA->IDR>>7)&1){
-	   		storenumber(11);
+        	   }
+        	   GPIOB->ODR^=(1U<<6);//PB6
 
-	   	}
-	   	GPIOA->ODR^=(1U<<2);//PA2
+        	   GPIOB->ODR^=(1U<<7);//PB7
+        	   if ((GPIOA->IDR>>7)&1){
+        		   storenumber(11);
 
-	   	GPIOA->ODR^=(1U<<3);//PA3
-	   	if ((GPIOA->IDR>>7)&1){
-	   		storenumber(11);
+        	   }
+        	   GPIOB->ODR^=(1U<<7);//PB7
 
-	   	}
-	   	GPIOA->ODR^=(1U<<2);//PA2
 
-        EXTI->RPR1|=(1U<<7);//clear interrupt flag
-        setRowsKeypad();
+               EXTI->RPR1|=(1U<<7);//clear interrupt flag
+               setRowsKeypad();
+
 
       }
 
@@ -161,20 +163,20 @@ void EXTI4_15IRQHandler(void){
 
 int main(void){
 
-RCC->IOPENR |=(1U<<0);/*Enable GPIOA clock*/
+RCC->IOPENR |=(1U<<0);/*Enable GPIOB clock*/
 
-/*Set up PA0,PA1,PA2,PA3 as output row(output=01)*/
-GPIOA->MODER &=~(3U<<0);
-GPIOA->MODER |=(1U<<0);/*for PA0*/
+/*Set up PB4,PB5,PB6,PB7 as output row(output=01)*/
+GPIOB->MODER &=~(3U<<8);
+GPIOB->MODER |=(1U<<8);/*for PB4*/
 
-GPIOA->MODER &=~(3U<<2);
-GPIOA->MODER |=(1U<<2);/*for PA1*/
+GPIOB->MODER &=~(3U<<10);
+GPIOB->MODER |=(1U<<10);/*for PB5*/
 
-GPIOA->MODER &=~(3U<<4);
-GPIOA->MODER |=(1U<<4);/*for PA2*/
+GPIOB->MODER &=~(3U<<12);
+GPIOB->MODER |=(1U<<12);/*for PB6*/
 
-GPIOA->MODER &=~(3U<<6);
-GPIOA->MODER |=(1U<<6);/*for PA3*/
+GPIOB->MODER &=~(3U<<14);
+GPIOB->MODER |=(1U<<14);/*for PA7*/
 
 
 /*Set up PA4,PA5,PA6,PA7 as input column(input=00)*/
@@ -217,10 +219,10 @@ NVIC_EnableIRQ(EXTI4_15_IRQn);
 
 
 /*Set all rows*//*tuşların hepsi aktif 5v oldu*/
-GPIOA->ODR |=(1U<<0);/*PA0*/
-GPIOA->ODR |=(1U<<1);/*PA1*/
-GPIOA->ODR |=(1U<<2);/*PA2*/
-GPIOA->ODR |=(1U<<3);/*PA3*/
+GPIOA->ODR |=(1U<<4);/*PB4*/
+GPIOA->ODR |=(1U<<5);/*PB5*/
+GPIOA->ODR |=(1U<<6);/*PB6*/
+GPIOA->ODR |=(1U<<7);/*PB7*/
 
 
 while(1){
@@ -233,48 +235,65 @@ return 0;
 
 
 void storenumber(int sayı){
-	if (sayı>=0 && sayı<=9){
+	if (sayı>=0 && sayı<=9 && numbers[0]!=0){
 		numbers[number]=sayı;
 		number=number+1;
 	}
+	else if(sayı>=0 && sayı<=9 && numbers[0]==0){
+		number=0;
+	}
 	else if(sayı==10 && number!=0){
         int i;
-        n=sizeof(number)-1;
-		for(i=0;i<sizeof(number)-1;i++){
-			ust=10^n;
+
+		for(i=0;i<number-1;i++){
+			ust=10^(number - i)-1;
 			count=count+numbers[i]*ust;
-			n=n-1;
 
 		}
 	    number=0;
+	    numbers[0]=0;
+	    numbers[1]=0;
+	    numbers[2]=0;
+	    numbers[3]=0;
+	    numbers[4]=0;
 
 	}
 
 	else if(sayı==10 && number==0){
 		number=0;
+		numbers[0]=0;
+		numbers[1]=0;
+		numbers[2]=0;
+		numbers[3]=0;
+		numbers[4]=0;
 	}
 	else{
 		number=0;
-	}
+	    numbers[0]=0;
+		numbers[1]=0;
+		numbers[2]=0;
+		numbers[3]=0;
+		numbers[4]=0;
+}
 
 
 }
 
 void clearRowsKeypad(void){
 	/*Clearing the rows*/
-	GPIOA->ODR &=~(1U<<0);/*PA0*/
-	GPIOA->ODR &=~(1U<<1);/*PA1*/
-	GPIOA->ODR &=~(1U<<2);/*PA2*/
-	GPIOA->ODR &=~(1U<<3);/*PA3*/
+	GPIOA->ODR &=~(1U<<4);/*PB4*/
+	GPIOA->ODR &=~(1U<<5);/*PB5*/
+	GPIOA->ODR &=~(1U<<6);/*PB6*/
+	GPIOA->ODR &=~(1U<<7);/*PB7*/
 
 }
 
 
 void setRowsKeypad(void){
 	/*Setting rows*/
-	GPIOA->ODR |=(1U<<0);/*PA0*/
-	GPIOA->ODR |=(1U<<1);/*PA1*/
-	GPIOA->ODR |=(1U<<2);/*PA2*/
-	GPIOA->ODR |=(1U<<3);/*PA3*/
+	GPIOA->ODR |=(1U<<4);/*PB4*/
+	GPIOA->ODR |=(1U<<5);/*PB5*/
+	GPIOA->ODR |=(1U<<6);/*PB6*/
+	GPIOA->ODR |=(1U<<7);/*PB7*/
 
 }
