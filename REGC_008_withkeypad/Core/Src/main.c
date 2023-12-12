18d548908029dd48bc66b96e02145096a1bef78b
busraulker <busraulker2929@gmail.com>
2023-12-12 13:01:35 +0300

    KEYPAD UPDATE

--- REGC_008_withkeypad/Core/Src/main.c
+++ REGC_008_withkeypad/Core/Src/main.c
@@ -163,73 +163,74 @@ void EXTI4_15IRQHandler(void){
 
 int main(void){
 
-RCC->IOPENR |=(1U<<0);/*Enable GPIOA clock*/
-RCC->IOPENR |=(1U<<1);/*Enable GPIOB clock*/
+   RCC->IOPENR |=(1U<<0);/*Enable GPIOA clock*/
+   RCC->IOPENR |=(1U<<1);/*Enable GPIOB clock*/
 
-/*Set up PB4,PB5,PB6,PB7 as output row(output=01)*/
-GPIOB->MODER &=~(3U<<8);
-GPIOB->MODER |=(1U<<8);/*for PB4*/
+   /*Set up PB4,PB5,PB6,PB7 as output row(output=01)*/
+   GPIOB->MODER &=~(3U<<8);
+   GPIOB->MODER |=(1U<<8);/*for PB4*/
 
-GPIOB->MODER &=~(3U<<10);
-GPIOB->MODER |=(1U<<10);/*for PB5*/
+   GPIOB->MODER &=~(3U<<10);
+   GPIOB->MODER |=(1U<<10);/*for PB5*/
 
-GPIOB->MODER &=~(3U<<12);
-GPIOB->MODER |=(1U<<12);/*for PB6*/
+   GPIOB->MODER &=~(3U<<12);
+   GPIOB->MODER |=(1U<<12);/*for PB6*/
 
-GPIOB->MODER &=~(3U<<14);
-GPIOB->MODER |=(1U<<14);/*for PA7*/
-
-
-/*Set up PA4,PA5,PA6,PA7 as input column(input=00)*/
-GPIOA->MODER &=~(3U<<8);/*for PA4*/
-GPIOA->PUPDR |=(2U<<8);/*Pull down mode*/
-
-GPIOA->MODER &=~(3U<<10);/*for PA5*/
-GPIOA->PUPDR |=(2U<<10);/*Pull down mode*/
-
-GPIOA->MODER &=~(3U<<12);/*for PA6*/
-GPIOA->PUPDR |=(2U<<12);/*Pull down mode*/
-
-GPIOA->MODER &=~(3U<<14);/*for PA7*/
-GPIOA->PUPDR |=(2U<<14);/*Pull down mode*/
+   GPIOB->MODER &=~(3U<<14);
+   GPIOB->MODER |=(1U<<14);/*for PB7*/
 
 
-/*Set up interrupt for input*/
-EXTI->EXTICR[1] |=(0U<<0);/*PA4*/
-EXTI->EXTICR[1] |=(0U<<8);/*PA5*/
-EXTI->EXTICR[1] |=(0U<<16);/*PA6*/
-EXTI->EXTICR[1] |=(0U<<24);/*PA7*/
+   /*Set up PA4,PA5,PA6,PA7 as input column(input=00)*/
+   GPIOA->MODER &=~(3U<<8);/*for PA4*/
+   GPIOA->PUPDR |=(2U<<8);/*Pull down mode*/
+
+   GPIOA->MODER &=~(3U<<10);/*for PA5*/
+   GPIOA->PUPDR |=(2U<<10);/*Pull down mode*/
+
+   GPIOA->MODER &=~(3U<<12);/*for PA6*/
+   GPIOA->PUPDR |=(2U<<12);/*Pull down mode*/
+
+   GPIOA->MODER &=~(3U<<14);/*for PA7*/
+   GPIOA->PUPDR |=(2U<<14);/*Pull down mode*/
 
 
-/*Rising edge*/
-EXTI->RTSR1|=(1U<<4);
-EXTI->RTSR1 |=(1U<<5);
-EXTI->RTSR1|=(1U<<6);
-EXTI->RTSR1|=(1U<<7);
-
-/*MASK*/
-EXTI->IMR1 |=(1U<<4);
-EXTI->IMR1 |=(1U<<5);
-EXTI->IMR1 |=(1U<<6);
-EXTI->IMR1 |=(1U<<7);
-
-/*NVIC*/
-
-NVIC_SetPriority(EXTI4_15_IRQn,0);
-NVIC_EnableIRQ(EXTI4_15_IRQn);
+   /*Set up interrupt for input*/
+   EXTI->EXTICR[2] |=EXTI_EXTICR2_EXTI4 ;/*PA4*/
+   EXTI->EXTICR[2] |=(0U<<8);/*PA5*/
+   EXTI->EXTICR[2] |=(0U<<16);/*PA6*/
+   EXTI->EXTICR[2] |=(0U<<24);/*PA7*/
 
 
-/*Set all rows*//*tuşların hepsi aktif 5v oldu*/
-GPIOA->ODR |=(1U<<4);/*PB4*/
-GPIOA->ODR |=(1U<<5);/*PB5*/
-GPIOA->ODR |=(1U<<6);/*PB6*/
-GPIOA->ODR |=(1U<<7);/*PB7*/
+   /*Rising edge*/
+   EXTI->RTSR1|=(1U<<4);
+   EXTI->RTSR1 |=(1U<<5);
+   EXTI->RTSR1|=(1U<<6);
+   EXTI->RTSR1|=(1U<<7);
+
+   /*MASK*/
+   EXTI->IMR1 |=(1U<<4);
+   EXTI->IMR1 |=(1U<<5);
+   EXTI->IMR1 |=(1U<<6);
+   EXTI->IMR1 |=(1U<<7);
 
 
-while(1){
+   /*NVIC*/
 
-}
-return 0;
+   NVIC_SetPriority(EXTI4_15_IRQn,0);
+   NVIC_EnableIRQ(EXTI4_15_IRQn);
+
+
+   /*Set all rows*//*tuşların hepsi aktif 5v oldu*/
+   GPIOB->ODR |=(1U<<4);/*PB4*/
+   GPIOB->ODR |=(1U<<5);/*PB5*/
+   GPIOB->ODR |=(1U<<6);/*PB6*/
+   GPIOB->ODR |=(1U<<7);/*PB7*/
+
+
+   while(1){
+
+    }
+    return 0;
 
 }
 
