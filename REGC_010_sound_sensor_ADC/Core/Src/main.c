b870bcec646ffa04dc9f384228bf2fd1d2684386
Fouad Aladhami <fouadiadhami@gmail.com>
2023-12-29 22:19:52 +0300

    Uart code added to sound sensor code to visualize the sound siggnal

--- REGC_010_sound_sensor_ADC/Core/Src/main.c
+++ REGC_010_sound_sensor_ADC/Core/Src/main.c
@@ -16,6 +16,10 @@ void RCC_init(void);
 void GPIOA_init(void);
 void ADC_init(void);
 uint16_t poll_ADC(void);
+void USART2_Init(void);
+void printChar(uint8_t c);
+int _print(int f, char *ptr, int len);
+void print(char *s);
 static inline float convert_to_voltage(uint16_t adc_value);
 // variables
 
@@ -24,6 +28,8 @@ typedef struct
 	uint16_t adc_value;
 	float voltage;
 } sound_sensor;
+
+char txbuffer[3] = "/0";
 
 sound_sensor soundSensor;
 int main(void)
@@ -39,6 +45,9 @@ int main(void)
 		soundSensor.adc_value = poll_ADC();
 		// Convert adc value to voltage
 		soundSensor.voltage = convert_to_voltage(soundSensor.adc_value);
+		sprintf(txbuffer, "%d", soundSensor.adc_value);
+		print(txbuffer);
+
 	}
 	return 0;
 }
@@ -47,6 +56,11 @@ void RCC_init(void)
 	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
 	// Enable ADC clock
 	RCC->APBENR2 |= RCC_APBENR2_ADCEN;
+
+	/************* Uart *************/
+	// enable GPIOA and USART2 module clocks from RCC
+	RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+	RCC->APBENR1 |= RCC_APBENR1_USART2EN;
 }
 
 void GPIOA_init(void)
@@ -61,6 +75,14 @@ void GPIOA_init(void)
 	GPIOA->OSPEEDR &= ~GPIO_OSPEEDR_OSPEED12;
 	// Pa12 pin as no pull up, no pull down
 	GPIOA->PUPDR &= ~GPIO_PUPDR_PUPD12;
+
+	/**********UART************/
+	// enable and setup pins PA2 and PA3 as alternate function for USART2 module
+	GPIOA->MODER &= ~(GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
+	GPIOA->MODER |= (GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1);
+	GPIOA->AFR[0] &= ~(GPIO_AFRL_AFSEL2 | GPIO_AFRL_AFSEL3);
+	GPIOA->AFR[0] |= (GPIO_AFRL_AFSEL2_0 | GPIO_AFRL_AFSEL3_0);
+
 }
 void ADC_init(void)
 {
@@ -86,6 +108,17 @@ void ADC_init(void)
 	// Select the ADC input channel
 	ADC1->CHSELR |= ADC_CHSELR_CHSEL16;
 }
+void USART2_Init(void)
+{
+	// enable receive and transmit from USART2 module
+	USART2->CR1 |= (USART_CR1_RE | USART_CR1_TE);
+	// set baud rate to 9600 bps assuming clock is running at 16Mhz
+	// Baud Rate = APBxCLK / USARTx->BRR
+	USART2->BRR = 0x683; // 1667
+	// enable uart module
+	USART2->CR1 |= USART_CR1_UE;
+}
+
 uint16_t poll_ADC(void)
 {
 	// Start the ADC conversion
@@ -100,4 +133,35 @@ uint16_t poll_ADC(void)
 static inline float convert_to_voltage(uint16_t adc_value)
 {
 	return (float)adc_value * VDDA / ADC_MAX_VALUE;
+}
+
+//Uart functions
+
+void printChar(uint8_t c)
+{
+	// write c to the transmit data register
+	USART2->TDR = c;
+	// wait until transmit is complete
+	while (!(USART2->ISR & USART_ISR_TC))
+		;
+}
+int _print(int f, char *ptr, int len)
+{
+	(void)f; // don't do anthing with f
+	// call printChar within a for loop len times
+	for (int i = 0; i < len; i++)
+	{
+		printChar(ptr[i]);
+	}
+	return len; // return length
+}
+void print(char *s)
+{
+	// count number of characters in s string until a null byte comes `\0`
+	int length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	_print(0, s, length);
 }
